2db79f6935fcf63fdc5bace3dcdf0952801c8a92
Sameer Karim <sameerkarim1@gmail.com>
2013-11-09 00:42:56 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help page" do
@@ -16,6 +21,24 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help') 
     end
-
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
+  
+  describe "About page" do
+    
+    it "should have the content 'about'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+
 end
